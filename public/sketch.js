48774fcc1b7575b90173578ea4184dca1df7f681
Sofia-Ortega <s.ortega@tamu.edu>
2020-11-26 13:28:41 -0600

    teleporting boundaries

--- public/sketch.js
+++ public/sketch.js
@@ -1,5 +1,9 @@
 hit = false;
 var x, y;
+var x1 = 0;
+var y1 = 150;
+var w1 = 150;
+var h1 = 50;
 
 function setup() {
     createCanvas(800, 600);
@@ -9,15 +13,18 @@ function setup() {
 }
 
 function draw() {
-    background(hit ? 100 : 0);
+    background(100);
 
-    fill(hit ? [255, 204, 0]: [0,255, 0] );
-    rect(200, 200, 100, 150)
+    fill(0);
+    rect(x1, y1, w1, h1)
+
+    fill([255, 204, 0]);
     circle(x, y, 100)
 
-    hit = collideRectCircle(200, 200, 100, 150, x, y, 100)
+    hit = collideRectCircle(x1, y1, w1, h1, x, y, 100)
 
     controls();
+    checkHit(hit)
 
     print('colliding?', hit);
 
@@ -28,13 +35,21 @@ function controls() {
     if(keyIsDown(38)) {
         y-=3;
     }
-    if(keyIsDown(40)) {
+    else if(keyIsDown(40)) {
         y+=3;
     }
     if(keyIsDown(37)) {
         x-=3;
     }
-    if(keyIsDown(39)) {
+    else if(keyIsDown(39)) {
+
         x+=3;
+    }
+}
+
+function checkHit(hit) {
+    if(hit) {
+        x = width/2;
+        y = height/2;
     }
 }
