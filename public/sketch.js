fa98f7839ad31b380bf4de6f85fc442737e91276
Sofia-Ortega <s.ortega@tamu.edu>
2020-11-26 00:14:53 -0600

    basic collision and key controls added

--- public/sketch.js
+++ public/sketch.js
@@ -1,12 +1,40 @@
-
+hit = false;
+var x, y;
 
 function setup() {
-    createCanvas(500, 400);
-
+    createCanvas(800, 600);
+    noStroke();
+    x = width/2;
+    y = height/2;
 }
 
 function draw() {
-    background(100);
-    ellipse(100, 100, 100, 100);
+    background(hit ? 100 : 0);
 
+    fill(hit ? [255, 204, 0]: [0,255, 0] );
+    rect(200, 200, 100, 150)
+    circle(x, y, 100)
+
+    hit = collideRectCircle(200, 200, 100, 150, x, y, 100)
+
+    controls();
+
+    print('colliding?', hit);
+
+}
+
+
+function controls() {
+    if(keyIsDown(38)) {
+        y-=3;
+    }
+    if(keyIsDown(40)) {
+        y+=3;
+    }
+    if(keyIsDown(37)) {
+        x-=3;
+    }
+    if(keyIsDown(39)) {
+        x+=3;
+    }
 }
